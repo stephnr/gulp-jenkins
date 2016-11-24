9f9819a0aceb3dad315b9731c16e3aefb42e4ed0
Stephen Rodriguez <Stephen@Stephens-MBP-2.home>
2016-11-24 14:11:44 -0500

    initial testing framework

---
+++ __tests__/jenkins.spec.js
@@ -0,0 +1,10 @@
+const chai = require('chai');
+const gulpJenkins = require('../');
+
+describe('Gulp Jenkins', function() {
+
+  it('should exist', function() {
+    chai.expect(gulpJenkins).to.exist;
+  });
+
+});
